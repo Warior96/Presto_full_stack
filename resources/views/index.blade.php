e7bea7d17fe0cec4032f9460c9214ae374a0a5ba
Davide Mozzillo <mozzillodavide@gmail.com>
2025-02-24 14:43:12 +0100

    inserimento grid system homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,13 @@
 <x-layout>
-    <h1>sono la homepage</h1>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <h1 class="text-center col-12">sono la homepage</h1>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                
+            </div>
+        </div>
+    </div>
 </x-layout>
