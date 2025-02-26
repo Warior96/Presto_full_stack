54701bde5cc059ee45fb827e6b5b2c6268046f11
Giacomo Hu <giacomohu05@gmail.com>
2025-02-26 16:58:08 +0100

    aggiunto la modade e sistemato un po di front

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -14,12 +14,49 @@
                         un articolo</a>
                 </div>
             @endauth
-            <h1 class="col-6 display-1 text-center mb-3">Presto.it</h1>
+            <h1 class="col-6 display-1 text-center mb-3">Emporium Shop</h1>
             @auth
-                <div class="col-3 d-flex align-items-center justify-content-end">
-                    <a href="{{ route('become.revisor') }}"
-                        class="btn btn-primary px-3 py-2 fs-5 rounded shadow">Candidati</a>
-                </div>
+                @if (Auth::user()->is_revisor != true)
+                   
+                    <div class="col-3 d-flex justify-content-end align-content-center ">
+                        <button type="button" class="btn btn-primary  my-4 fs-5 rounded shadow" data-bs-toggle="modal"
+                            data-bs-target="#staticBackdrop">
+                            Lavora con noi
+                        </button>
+                        <!-- Modal -->
+                        <div class="modal fade " id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false"
+                            tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-dialog-centered ">
+                                <div class="modal-content ">
+                                    <div class="modal-header">
+                                        <h1 class="modal-title fs-5 w-100 pt-3" id="staticBackdropLabel">
+                                            {{ Auth::user()->name }}, vuoi lavorare con noi?
+                                        </h1>
+                                        <button type="button" class="btn-close bg-white" data-bs-dismiss="modal"
+                                            aria-label="Close"></button>
+                                    </div>
+                                    <div class="modal-body px-4">
+                                        <p>I tuoi dati:</p>
+                                        <ul>
+                                            <li>nome: {{ Auth::user()->name }}</li>
+                                            <li>email: {{ Auth::user()->email }}</li>
+                                        </ul>
+                                    </div>
+                                    <div class="modal-footer d-flex justify-content-between align-items-center pb-2">
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">No,
+                                            annulla</button>
+                                        <a href="{{ route('become.revisor') }}"
+                                            class="btn btn-primary px-3 py-2  rounded shadow">Candidati</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    @else
+                    <div class="col-3 d-flex justify-content-end align-content-center">
+                        <h1>Sei</h1>
+                        </div>
+                @endif
             @endauth
             {{-- MESSAGGIO DI SUCCESSO PER LA CANDIDATURA COME REVISORE --}}
 
