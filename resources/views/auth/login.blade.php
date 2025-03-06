12411b983d23d6bce7aab5d08bbc0760f8a9b71d
Davide Nicolò Mozzillo <mozzillodavide@gmail.com>
2025-03-06 15:23:23 +0100

    inserite validazioni personalizzate e tradotte per login e register, sistemato front login e register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,27 +2,64 @@
     <header class="container mt-5 pt-5 vh-100">
         <div class="row justify-content-center pt-3">
             <h1 class="col-12 display-4 text-center mb-3">Login</h1>
-            <div class="col-6">
+            {{-- <div class="col-6">
                 <x-error></x-error>
-            </div>
+            </div> --}}
         </div>
 
         <div class="row justify-content-center mt-5">
-            <form method="POST" action="{{ route('login') }}" class="col-6 col-md-8 col-lg-6 rounded shadow bg-1 px-5 py-4">
+            <form method="POST" action="{{ route('login') }}"
+                class="col-6 col-md-8 col-lg-6 rounded shadow bg-1 px-5 py-4">
                 @csrf
                 <div class="mb-3">
                     <label class="form-label" for="emailLogin">Email</label>
                     <input type="email" class="form-control" name="email" id="emailLogin"
                         value="{{ old('email') }}">
+                    @error('email')
+                        <div class="px-2 py-1 fst-italic bg-danger-subtle rounded mt-1">
+                            @if ($message == 'The email field is required.' && session('locale') == 'it')
+                                Il campo email è obbligatorio.
+                            @elseif ($message == 'The email field is required.' && session('locale') == 'zh-tw')
+                                必须填写邮箱
+                            @elseif ($message == 'These credentials do not match our records.' && session('locale') == 'it')
+                                Email e password non corrispondono a nessun utente registrato.
+                            @elseif ($message == 'These credentials do not match our records.' && session('locale') == 'zh-tw')
+                                邮箱密码与我们的数据不匹配
+                            @elseif ($message == 'The email must be a valid email address.' && session('locale') == 'it')
+                                L'email deve essere un indirizzo email valido.
+                            @elseif ($message == 'The email must be a valid email address.' && session('locale') == 'zh-tw')
+                                邮箱不存在
+                            @else
+                                {{ $message }}
+                            @endif
+                        </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label class="form-label" for="passwordLogin">Password</label>
                     <input type="password" class="form-control" name="password" id="passwordLogin">
+                    @error('password')
+                        <div class="px-2 py-1 fst-italic bg-danger-subtle rounded mt-1">
+                            @if ($message == 'The password field is required.' && session('locale') == 'it')
+                                Il campo password è obbligatorio.
+                            @elseif ($message == 'The password field is required.' && session('locale') == 'zh-tw')
+                                必须填写密码
+                            @elseif ($message == 'The password is incorrect.' && session('locale') == 'it')
+                                La password è errata.
+                            @elseif ($message == 'The password is incorrect.' && session('locale') == 'zh-tw')
+                                密码错误
+                            @else
+                                {{ $message }}
+                            @endif
+                        </div>
+                    @enderror
                 </div>
-                <button type="submit" class="btn btn-cus bg-4 w-100 p-3 mt-4 fs-5" id="btn-login">{{ __('ui.login') }}</button>
+                <button type="submit" class="btn btn-cus bg-4 w-100 p-3 mt-4 fs-5"
+                    id="btn-login">{{ __('ui.login') }}</button>
 
                 <div class="text-center mt-4">
-                    <a href="{{ route('register') }}" class="text-decoration-none c-2 fs-5">Non sei registrato? Registrati</a>
+                    <a href="{{ route('register') }}" class="text-decoration-none c-2 fs-5">Non sei registrato?
+                        Registrati</a>
                 </div>
 
             </form>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,13 +2,11 @@
     <header class="container mt-5 pt-5 min-vh-100">
         <div class="row justify-content-center">
             <h1 class="col-12 display-4 text-center mb-3">{{ __('ui.registrati') }}</h1>
-            <div class="col-6">
-                <x-error></x-error>
-            </div>
         </div>
 
         <div class="row justify-content-center mt-3">
-            <form method="POST" action="{{ route('register') }}" class="col-6 col-md-8 col-lg-6 rounded shadow bg-1 px-5 py-4">
+            <form method="POST" action="{{ route('register') }}"
+                class="col-6 col-md-8 col-lg-6 rounded shadow bg-1 px-5 py-4">
                 @csrf
                 <div class="mb-3">
                     <label class="form-label" for="nameRegister">Username</label>
@@ -19,17 +17,33 @@
                     <label for="emailRegister" class="form-label">Email </label>
                     <input type="email" class="form-control" id="emailRegister" name="email"
                         value="{{ old('email') }}">
+                    @error('email')
+                        <div class="px-2 py-1 fst-italic bg-danger-subtle rounded mt-1">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="passwordRegister" class="form-label">Password</label>
                     <input type="password" class="form-control" id="passwordRegister" name="password">
+                    @error('password')
+                        <div class="px-2 py-1 fst-italic bg-danger-subtle rounded mt-1">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
                 <div class="mb-3">
                     <label for="passwordRegisterConfirmation" class="form-label">Conferma la password</label>
                     <input type="password" class="form-control" id="passwordRegisterConfirmation"
                         name="password_confirmation">
+                    @error('password_confirmation')
+                        <div class="px-2 py-1 fst-italic bg-danger-subtle rounded mt-1">
+                            {{ $message }}
+                        </div>
+                    @enderror
                 </div>
-                <button type="submit" class="btn btn-cus bg-4 w-100 p-3 mt-4 fs-5" id="btn-register">{{ __('ui.registrati') }}</button>
+                <button type="submit" class="btn btn-cus bg-4 w-100 p-3 mt-4 fs-5"
+                    id="btn-register">{{ __('ui.registrati') }}</button>
                 <div class="text-center mt-4">
                     <a href="{{ route('login') }}" class="text-decoration-none c-2 fs-5">Sei gia registrato? Loggati</a>
                 </div>
